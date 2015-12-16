ab776e04954be1e4eae5f18bac0bb22714e00048
Max <maxDmayhew@gmail.com>
2015-12-16 02:55:19 -0500

    adding scrollify -having some weird issues with the initial scroll...just going to see what it does live

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
 	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
+        $.scrollify({
+                section: ".panel",
+            });
+        	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
