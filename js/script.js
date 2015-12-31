b405e34cbce1ec6a3fe20e7ef54f4b87b3836d42
Max <maxDmayhew@gmail.com>
2015-12-31 00:48:55 -0500

    various edits...main js file might break everything with this change though.

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ $(document).ready(function() {
     
     //nothing to see here >_
         
-        	  $('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top
-	        }, 750);
-	        return false;
-	      }
-	    }
-	  });
+//        	  $('a[href*=#]:not([href=#])').click(function() {
+//	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//	      var target = $(this.hash);
+//	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//	      if (target.length) {
+//	        $('html,body').animate({
+//	          scrollTop: target.offset().top
+//	        }, 750);
+//	        return false;
+//	      }
+//	    }
+//	  });
     //end func    
 	});
    
